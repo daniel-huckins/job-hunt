a054cf2cf70b49740028f3bf110857f69aa4cdca
Daniel Huckins <daniel.huckins@gmail.com>
2016-10-03 14:02:31 -0400

    seperate client/server

---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+'use strict'
+
+const webpack = require('webpack')
+
+module.exports = {
+  entry: './client/index.js',
+  output: {
+    path: `${__dirname}/client`, filename: 'bundle.js'
+  },
+  module: {
+    loaders: [
+      {
+        test: /.jsx?$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/,
+        query: {
+          presets: ['es2015', 'react']
+        }
+      }
+    ]
+  },
+  plugins: [
+    new webpack.optimize.UglifyJsPlugin({
+      compress: {
+        warnings: false
+      }
+    })
+  ]
+}
